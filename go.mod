6b16cdec3896576fdb77930b07cd792387200e6f
gaurav1952 <gaurav05655@gmail.com>
2024-09-22 11:47:42 +0530

    inital commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module notes
+
+go 1.22.3
+
+require (
+	github.com/fatih/color v1.17.0
+	github.com/mattn/go-sqlite3 v1.14.23
+)
+
+require (
+	github.com/jedib0t/go-pretty/v6 v6.5.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/jedib0t/go-pretty/v6/table"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func checkerr(err error) {
+	if err != nil {
+		log.Printf("error: %v", err)
+		log.Fatal(err)
+
+	}
+}
+
+func initdb() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
+
+func main() {
+	// color design
+	helpStyle_underline := color.New(color.FgHiCyan).Add(color.Underline).SprintFunc()
+	helpStyle_notes_func_command := color.New(color.FgGreen).SprintFunc()
+
+	db, err := initdb()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	statement, err := db.Prepare(`create table if not exists notes(id integer not null primary key autoincrement , note text ,  created_date  date  , created_time time default current_time );`)
+	checkerr(err)
+	statement.Exec()
+
+	if len(os.Args) == 1 {
+		// .Add(color.Underline)
+		c := color.New(color.FgRed).Add(color.Bold)
+		c.Println("No valid command provided. Try 'notes help' for usage.")
+	}
+
+	if len(os.Args) > 1 {
+
+		for _, v := range os.Args {
+
+			if v == "help" {
+				fmt.Println("Usage:")
+				fmt.Println("  notes [command]")
+				fmt.Println()
+				fmt.Println("Available commands:")
+				fmt.Println(helpStyle_underline("help") + "   - Show this help message")
+				fmt.Println(helpStyle_notes_func_command("add") + " -  Add notes (Enter to type notes)")
+				fmt.Println(helpStyle_notes_func_command("show") + " -  Show notes  ")
+				fmt.Println(helpStyle_notes_func_command("rm") + " -  Delete notes ")
+				return
+			}
+
+			if v == "rm" {
+
+				note_delete, err := db.Prepare("DELETE FROM notes WHERE id = ?")
+				checkerr(err)
+				_, err = note_delete.Exec(os.Args[2])
+				checkerr(err)
+
+			}
+
+			if v == "add" {
+				reader := bufio.NewReader(os.Stdin)
+				fmt.Print(":")
+				user_notes, err := reader.ReadString('\n')
+				checkerr(err)
+
+				currentTime := time.Now()
+
+				date := currentTime.Format("2006-01-02")
+
+				statement, err = db.Prepare("INSERT INTO notes (note , created_date) VALUES (?,?)")
+				checkerr(err)
+				_, execErr := statement.Exec(user_notes, date)
+				checkerr(execErr)
+
+			}
+
+			if v == "show" {
+				rows, err := db.Query("select id, note, created_date, created_time from notes")
+				checkerr(err)
+				headerStyle := color.New(color.FgCyan, color.Bold).SprintFunc()
+
+				notes_table := table.NewWriter()
+				notes_table.SetOutputMirror(os.Stdout)
+				notes_table.AppendHeader(table.Row{
+					headerStyle("#"),
+					headerStyle("Note"),
+					headerStyle("Created Date"),
+					headerStyle("Created Time"),
+				})
+
+				var id string
+				var note string
+
+				var created_date string
+				var created_time string
+				for rows.Next() {
+					rows.Scan(&id, &note, &created_date, &created_time)
+
+					// for i := 1; i < len(id); i++ {
+
+					// }
+					if len(created_date) >= 10 {
+						created_date = created_date[:10]
+					}
+					notes_table.AppendRow(table.Row{id, note, created_date, created_time})
+
+				}
+				notes_table.SetStyle(table.StyleRounded)
+				notes_table.Render()
+
+			}
+		}
+
+	}
+
+}
